60c2e8bdabdad2c60f971b2597e9c47591eae1c7
Bruno Alejandro <brunoalejandrodev@gmail.com>
2023-10-25 11:27:55 -0300

    correções gerais

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const btnLogin = document.querySelector('#btn_login')
+const input = document.querySelectorAll('.campoInput')
+
+input.forEach(campos =>{
+    campos.addEventListener('input', verificarInput)
+})
+
+function verificarInput(){
+    let todosPreenchidos = true;
+
+    input.forEach(campos =>{
+        if(campos.value.trim() === ''){
+            todosPreenchidos = false
+            console.log('erro')
+            return
+        }
+    });
+
+    if(todosPreenchidos){
+        btnLogin.removeAttribute('disabled')
+        btnLogin.classList.remove('botao-desabilitado')
+        btnLogin.classList.add('botao-habilitado')
+    }
+    else{
+        btnLogin.setAttribute('disabled', 'disabled')
+        btnLogin.classList.remove('botao-habilitado')
+        btnLogin.classList.add('botao-desabilitado')
+    }
+}
